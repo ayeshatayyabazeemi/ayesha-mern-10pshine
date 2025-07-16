5e53cd59b5eff46d76d1a631f1f3558840ce5452
ayeshatayyabazeemi <148626090+ayeshatayyabazeemi@users.noreply.github.com>
2025-07-16 13:16:49 +0500

    remove commented code from Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -18,30 +18,7 @@ const Dashboard = () => {
     navigate('/add-note');
   };
 
-//   useEffect(() => {
-//   const fetchNotes = async () => {
-//     const token = localStorage.getItem('jwtToken');
-//     console.log(token);
-//     if (!token || !user?.id) {
-//       console.warn('Missing token or user ID');
-//       return;
-//     }
 
-//     try {
-//       const res = await axios.get(`http://localhost:5000/api/note/read?user=${user.id}`, {
-//         headers: {
-//           Authorization: `Bearer ${token}`
-//         }
-//       });
-//       console.log(res);
-//       setNotes(res.data.notes || []);
-//     } catch (err) {
-//       console.error('Failed to fetch notes:', err);
-//     }
-//   };
-
-//   if (user?.username) fetchNotes();
-// }, [user]);
 
 
   return (
@@ -64,25 +41,6 @@ const Dashboard = () => {
 
       <button onClick={addNote} className="add-note-button">+ Note</button>
     </div>
-
-   
-   {/* <div className="notes-container">
-  {notes.length > 0 ? (
-    notes.map((note) => (
-      <div key={note._id} className="note-card">
-        <h3>{note.subject}</h3>
-      
-        <div
-          className="note-content"
-          dangerouslySetInnerHTML={{ __html: note.note }}
-        />
-        <small>{new Date(note.createdAt).toLocaleString()}</small>
-      </div>
-    ))
-  ) : (
-    <p className="no-notes">No notes to display.</p>
-  )}
-</div> */}
 
   </div>
 );
