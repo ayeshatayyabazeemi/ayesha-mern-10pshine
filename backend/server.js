571052e2bbd87f582a2e1c032c4d0975bec02f21
Ayesha Tayyab Azeemi <148626090+ayeshatayyabazeemi@users.noreply.github.com>
2025-08-02 00:06:22 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-// const noteRoutes = require('./routes/noteRoutes');
-// const authRoutes = require('./routes/authRoutes');
-// const pinoHttp=require('pino-http')
-
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use(pinoHttp());
-// app.use('/api/auth', authRoutes);
-// app.use('/api/note',noteRoutes);
-
-
-// module.exports = app;
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => app.listen(5000, () => console.log('✅ Server running on port 5000')))
-// .catch(err => console.error('❌ MongoDB connection error:', err));
-
 
 const express = require('express');
 const mongoose = require('mongoose');
